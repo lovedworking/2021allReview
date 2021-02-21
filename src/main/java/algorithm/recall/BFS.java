580aba889d32c2bbf79a37fa79697d44cbd11761
tukk <tukk@yonyou.com>
2021-02-21 12:39:11 +0800

    密码箱解锁 初版BFS

--- src/main/java/algorithm/recall/BFS.java
+++ src/main/java/algorithm/recall/BFS.java
@@ -1,5 +1,10 @@
 package algorithm.recall;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
 public class BFS {
     /**
      *
@@ -51,4 +56,72 @@ public class BFS {
      *
      *
      */
+
+    public int  openLock(String[] deadends,String target){
+        //记录需要跳过的死亡密码；
+        Set<String> deads = new HashSet<>();
+        for (String dead : deads) {
+            deads.add(dead);
+        }
+        //记录已经穷举过的密码，防止走回头路；
+        Set<String> visited=new HashSet<>();
+        Queue<String> q=new LinkedList<>();
+        //从起点开始扩散
+        int step=0;
+        q.offer("0000");
+        visited.add("0000");
+
+        while(!q.isEmpty()){
+            int sz=q.size();
+            //将当前队列中的所有节点向周围扩散
+            for (int i = 0; i < sz; i++) {
+                String cur=q.poll();
+                //判断是否到达终点
+                if(deads.contains(cur)){
+                    continue;
+                }
+                if(cur.equals(target)){
+                    return  step;
+                }
+
+                for (int j = 0; j < 4; j++) {
+                    String up=plusOne(cur,j);
+                    if(!visited.contains(up)){
+                        q.offer(up);
+                        visited.add(up);
+                    }
+                    String down=minusOne(cur,j);
+                    if(!visited.contains(down)){
+                        q.offer(down);
+                        visited.add(down);
+                    }
+                }
+
+            }
+            //增加步数  齐头并进
+            step++;
+        }
+        //穷举完没找到目标
+        return -1;
+    }
+
+    private String minusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '0')
+            ch[j] = '9';
+        else
+            ch[j] -= 1;
+        return new String(ch);
+    }
+
+    private String plusOne(String s, int j) {
+        char[] ch = s.toCharArray();
+        if (ch[j] == '9')
+            ch[j] = '0';
+        else
+            ch[j] += 1;
+        return new String(ch);
+    }
+
+
 }
